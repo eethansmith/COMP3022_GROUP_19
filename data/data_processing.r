cb03349aac8b1e55159379e13fe75ef5d8c27735
psyes9 <ethan.a.smith@hotmail.co.uk>
2025-03-13 12:41:57 +0000

    Addition of data text explaination

--- data/data_processing.r
+++ data/data_processing.r
@@ -1,2 +1,7 @@
+df <- read.csv("data/resources/mc1-report-data.csv")
 # Take column 'time' 
     # split YYYY-MM-DD hh:mm:ss into two columns (date and time)
+df$date <- as.Date(df$timestamp_column)
+df$time <- format(as.POSIXct(df$timestamp_column), format = "%H:%M:%S")
+
+write.csv(df, "mc1-report-data-append.csv", row.names = FALSE)
