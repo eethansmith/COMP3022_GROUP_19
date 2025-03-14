7fc76127b033cc77e4e508570d7321dd95c62aaa
psyes9 <ethan.a.smith@hotmail.co.uk>
2025-03-14 09:11:28 +0000

    Time column split into two columns for timestamp and date

--- data/data_processing.r
+++ data/data_processing.r
@@ -1,8 +1,12 @@
-# test push to data processing branch
+# Read in the original dataset 
 df <- read.csv("data/resources/mc1-report-data.csv")
 # Take column 'time' 
     # split YYYY-MM-DD hh:mm:ss into two columns (date and time)
-df$date <- as.Date(df$timestamp_column)
-df$time <- format(as.POSIXct(df$timestamp_column), format = "%H:%M:%S")
+df$date <- as.Date(df$time)
+df$timestamp <- format(as.POSIXct(df$time), "%H:%M:%S")
 
-write.csv(df, "mc1-report-data-append.csv", row.names = FALSE)
+# Remove 'time' column with original formating
+df$time <- NULL
+
+# Save data to new csv with changed column
+write.csv(df, "data/resources/mc1-report-data-processed.csv", row.names = FALSE)
