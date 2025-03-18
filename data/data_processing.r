a2d64f9a1b18524ff3d3eafd7148b89ec3716f43
psyes9 <ethan.a.smith@hotmail.co.uk>
2025-03-18 11:06:25 +0000

    Updated timestamp and day columns in processed data

--- data/data_processing.r
+++ data/data_processing.r
@@ -1,3 +1,5 @@
+library(dplyr)
+
 # Read in the original dataset 
 df <- read.csv("data/resources/mc1-report-data.csv")
 # Take column 'time' 
@@ -7,6 +9,9 @@ df$timestamp <- format(as.POSIXct(df$time), "%H:%M:%S")
 
 # Remove 'time' column with original formating
 df$time <- NULL
+
+df <- df %>%
+  select(date, timestamp, everything())
 
 # Save data to new csv with changed column
 write.csv(df, "data/resources/mc1-report-data-processed.csv", row.names = FALSE)
