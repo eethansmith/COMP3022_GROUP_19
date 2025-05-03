8bb7cf3791eae3286693650751bf7bb34c85d014
JcbSm <49797894+JcbSm@users.noreply.github.com>
2025-05-03 13:59:03 +0100

    feat(Q3): started layout for Question 3 page.

--- app/src/pages/Q3/Q3.jsx
+++ app/src/pages/Q3/Q3.jsx
@@ -1,5 +1,13 @@
 import Header from "../../components/Header";
+import Tabular, { TabFactory } from "../../components/Tabular";
 import styles from "./Q3.module.css";
+import UncertaintyByDayTab from "./Tabs/UncertaintyByDayTab";
+
+/**
+ * Creates a tab for the day specified
+ * @param {string} day 
+ */
+const DayTabFactory = (day, i, arr) => TabFactory(`Day ${day}`, <UncertaintyByDayTab day={day} />)
 
 const Question3 = () => {
 
@@ -8,8 +16,11 @@ const Question3 = () => {
 
             <Header />
 
-            <h3>How do conditions change over time? How does uncertainty in change over time? Describe the key changes you see.
-            </h3>
+            <h3>How do conditions change over time? How does uncertainty in change over time? Describe the key changes you see.</h3>
+
+            <div className={styles.tabs}>
+                <Tabular tabs={[1, 2, 3].map(DayTabFactory)} />
+            </div>
 
         </div>
     )
---
+++ app/src/pages/Q3/Tabs/UncertaintyByDayTab.jsx
@@ -0,0 +1,9 @@
+export default function UncertaintyByDayTab({
+    day,
+    ...props
+}) {
+
+    return (<div>
+        <h1>Uncertainty data for Day {day}</h1>
+    </div>)
+}
