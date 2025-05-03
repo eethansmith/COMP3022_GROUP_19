41644e1f043ca6d49fecbad8e5dd678ff7674243
JcbSm <49797894+JcbSm@users.noreply.github.com>
2025-05-03 15:59:00 +0100

    feat(Q3): Page structure

--- app/src/pages/Q3/Tabs/UncertaintyByDayTab.jsx
+++ app/src/pages/Q3/Tabs/UncertaintyByDayTab.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import styles from "./UncertaintyByDayTab.module.css";
+
 export default function UncertaintyByDayTab({
     day,
     ...props
 }) {
 
+    const [selected, setSelected] = useState();
+    
     return (<div>
         <h1>Uncertainty data for Day {day}</h1>
+
+        <div className={styles["body"]}>
+            <div className={styles["grid-container"]}>
+
+                <div className={styles["radar-diagram"]} />
+                <div className={styles["line-graph"]} />
+                <div className={styles["box-plot"]} />
+
+            </div>
+        </div>
     </div>)
 }
