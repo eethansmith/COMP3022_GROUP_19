9d489ea83e1a13cb9873b85e7ddc7ec1ef05f15b
psyes9 <ethan.a.smith@hotmail.co.uk>
2025-05-04 14:25:26 +0100

    roll back to pre merge

--- app/src/pages/Q2/Q2.jsx
+++ app/src/pages/Q2/Q2.jsx
@@ -1,37 +1,19 @@
-import React from "react";
 import Header from "../../components/Header";
-import Tabular, { TabFactory } from "../../components/Tabular";
 import styles from "./Q2.module.css";
 
-import makeServicesTab from "./Tabs/makeServicesTab";
+const Question2 = () => {
 
-const services = [
-    { label: "Rescue Team",     slug: "rescue"    },
-    { label: "Medical Services",slug: "medical"   },
-    { label: "Utilities",       slug: "utilities" },
-    { label: "Traffic Control", slug: "traffic"   }
-  ];
-  
-  const tabs = services.map(({ label, slug }) =>
-    TabFactory(label, makeServicesTab(slug))
-  );
-  
-  export default function Question1() {
     return (
-      <div className={styles.body}>
-        <Header />
-  
-        <h3>
-          "Emergency responders will base their initial response on the earthquake
-          shake map. Use visual analytics to determine how their response should
-          change based on damage reports from citizens on the ground. How would
-          you prioritise neighbourhoods for response? Which parts of the city are
-          hardest hit?"
-        </h3>
-  
-        <div className={styles.tabs}>
-          <Tabular tabs={tabs} />
+        <div className={styles["body"]}>
+
+            <Header />
+
+            <h3>Use visual analytics to show uncertainty in the data. Compare the reliability of neighborhood reports. Which neighborhoods are providing reliable reports? Provide a rationale for your response.
+            </h3>
+
         </div>
-      </div>
-    );
-  }
+    )
+
+};
+
+export default Question2;
