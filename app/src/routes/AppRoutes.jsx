99f6762096abbfe891fc1dcc32e2a44f4b957034
JcbSm <49797894+JcbSm@users.noreply.github.com>
2025-05-06 02:34:20 +0100

    cleanup

--- app/src/routes/AppRoutes.jsx
+++ app/src/routes/AppRoutes.jsx
@@ -1,6 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "../pages/Dashboard/Dashboard";
-import ExamplePage from "../pages/ExamplePage/ExamplePage";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Question1 from "../pages/Q1/Q1";
 import Question2 from "../pages/Q2/Q2";
 import Question3 from "../pages/Q3/Q3";
@@ -9,9 +7,7 @@ const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/example" element={<ExamplePage />} />
-        
+      <Route path="/" element={<Navigate to="/Q1" replace />} />
         <Route path="/Q1" element={<Question1 />} />
         <Route path="/Q2" element={<Question2 />} />
         <Route path="/Q3" element={<Question3 />} />
