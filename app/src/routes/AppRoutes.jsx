40a8745a069bf8bcf9ce9bbd57936a4672b12c7b
psyes9 <ethan.a.smith@hotmail.co.uk>
2025-05-13 00:21:45 +0100

    fixed react router error

--- app/src/routes/AppRoutes.jsx
+++ app/src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Question1 from "../pages/Q1/Q1";
 import Question2 from "../pages/Q2/Q2";
 import Question3 from "../pages/Q3/Q3";
